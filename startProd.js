0e6499b960274a1705e4381af4f84108627cbf23
Мусаев Амир Мусаевич <AMMusaev@sberbank.ru>
2022-04-18 21:56:15 +0300

    feat: deploy

--- startProd.js
+++ startProd.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const setupServer = require('./server');
@@ -5,4 +7,11 @@ const setupServer = require('./server');
 const app = express();
 
 setupServer(app);
+
+if (fs.existsSync('editor')) {
+  app.use('/editor', express.static('editor'));
+} else if (fs.existsSync('build/editor')) {
+  app.use('/editor', express.static('build/editor'));
+}
+
 app.listen(3000);
